6440d8ceb78c7a6bd01963af65b2f7f2b930102c
khmerlee <khmer_lee5535@live.com>
2017-11-01 21:22:50 +0800

    Ruby on Rails controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  #get 'greeter/hello'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# SAME AS ABOVE
+  get 'greeter/hello' => "greeter#hello"
+  get 'greeter/goodbye'
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 end
